bd9af63121de49d080fe1b5829c7a0bb330da74b
FairusKN <kamalnapidfairus@gmail.com>
2025-09-30 08:09:34 +0700

    Feat: Add Test Endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function() {
-    return view("welcome");
+return response()->json([
+    "message" => "it's working"
+], 201);
 });
 
 Route::prefix("auth")->group(function () {
