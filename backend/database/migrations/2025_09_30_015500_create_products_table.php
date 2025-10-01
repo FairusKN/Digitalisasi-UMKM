b057657daa3faaedb4fc11ecb4dd3fb6d9339d12
FairusKN <kamalnapidfairus@gmail.com>
2025-10-01 17:46:48 +0700

    Fix: Add Unique Name

--- backend/database/migrations/2025_09_30_015500_create_products_table.php
+++ backend/database/migrations/2025_09_30_015500_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->uuid("id")->primary();
-            $table->string("name");
+            $table->string("name")->unique();
             $table->decimal("price", 12,2);
             $table->boolean("is_available")->default(true);
             $table->string("image_path");
