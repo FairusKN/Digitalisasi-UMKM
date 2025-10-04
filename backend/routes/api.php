7b30acdb389f1eb10288c9831e1008df34310d6c
FairusKN <kamalnapidfairus@gmail.com>
2025-10-04 18:52:39 +0700

    Fix: Use  REsource

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,27 +22,8 @@ Route::middleware(['auth:sanctum', 'checktoken'])->group(function () {
     });
 
     Route::prefix("manager")->middleware("role:manager")->group(function () {
-        Route::prefix("products")->group(function() {
-            Route::get('/', [ProductController::class, "index"]);
-            Route::get('/{id}', [ProductController::class, "show"])
-                ->whereUuid('id');
-            Route::post('/', [ProductController::class, 'store']);
-            Route::put('/{id}', [ProductController::class, 'update'])
-                ->whereUuid('id');
-            Route::delete('/{id}', [ProductController::class, 'destroy'])
-                ->whereUuid('id');
-        });
-
-        Route::prefix("users")->group(function() {
-            Route::get("/", [UserController::class, "index"]);
-            Route::get("/{id}", [UserController::class, "show"])
-                ->whereUuid("id");
-            Route::post("/", [UserController::class, "store"]);
-            Route::put("/{id}", [UserController::class, "update"])
-                ->whereUuid("id");
-            Route::delete("/{id}", [UserController::class, "destroy"])
-                ->whereUuid("id");
-        });
+        Route::apiResource("products", ProductController::class)->whereUuid("product");
+        Route::apiResource("users", UserController::class)->whereUuid("user");
     });
 
 });
