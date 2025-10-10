c04918cccf3da5aadad4a842bea0180d2516a729
FairusKN <kamalnapidfairus@gmail.com>
2025-10-10 16:10:16 +0700

    Chore: Delete stuff

--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
